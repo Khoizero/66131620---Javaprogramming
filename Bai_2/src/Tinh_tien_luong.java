04ca6ab82c2a8c23c856259eaa40e72db976fbdd
Khoizero <trongxnguyen150@gmail.com>
2026-03-02 19:40:01 +0700

    Bai_2

---
+++ Bai_2/src/Tinh_tien_luong.java
@@ -0,0 +1,22 @@
+public class Tinh_tien_luong {
+    public static void main(){
+
+        //Nhập số giờ làm và lương theo giờ
+        double So_gio_lam = 60;
+        double Luong_theo_h = 200000;
+        double Tong_luong;
+
+        //Tính tổng lương & Nếu làm trên 40h những giờ vượt tính 1.5 lần
+        if (So_gio_lam <= 40){
+            Tong_luong = So_gio_lam * Luong_theo_h;
+        } else {
+            double Gio_bth = 40;
+            double Gio_vm = So_gio_lam - Gio_bth;
+
+            Tong_luong = (Gio_bth * Luong_theo_h) + (Gio_vm * Luong_theo_h * 1.5);
+        }
+
+        //Xuất kết quả
+        System.out.printf("Tổng tiền lương nhân viên nhận được là: %2f\n", Tong_luong);
+    }
+}
