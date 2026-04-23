e6e450efa4f9b48be1720d216e8e9244d9e34e4c
Khoizero <trongxnguyen150@gmail.com>
2026-04-23 07:55:40 +0700

    Kiểm tra giữa kì Câu 2
    Lần thứ nhất

---
+++ QuanLySV/src/thigk2/NguyenTrongKhoi/Run.java
@@ -0,0 +1,9 @@
+package thigk2.NguyenTrongKhoi;
+import java.util.ArrayList;
+
+public class Run {
+    public static void main(String[] args) {
+        //Tạo danh sách
+        ArrayList<SinhVien> 
+    }
+}
---
+++ QuanLySV/src/thigk2/NguyenTrongKhoi/SinhVien.java
@@ -0,0 +1,51 @@
+package thigk2.NguyenTrongKhoi;
+
+public class SinhVien {
+    //Khi báo thuộc tính
+    private int mssv;
+    private String hoTen;
+    private int namSinh;
+    private boolean gioiTinh;
+
+    //Constructor
+    public SinhVien() {}
+    public SinhVien(int mssv, String hoTen, int namSinh, boolean gioiTinh) {
+        this.mssv = mssv;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.gioiTinh = gioiTinh;
+    }
+
+    //getters và setters
+    public int getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(int mssv) {
+        this.mssv = mssv;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(int namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+}
